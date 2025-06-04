2ba65d979dbf3a31e678de9c37b649e4ce892013
alssandro-ui <alessandro@email.com>
2025-06-04 14:15:50 -0300

    inserção de script para dropdown de perfil

--- blog.php
+++ blog.php
@@ -155,5 +155,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["acao"]) && $_POST["aca
         </div>
     </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous">
+</script>
 
 </html>
